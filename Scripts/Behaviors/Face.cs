646d5dee019cfd73fed370cc4381dccda4e3f8a9
jojax1902 <49213703+jojax1902@users.noreply.github.com>
2021-01-31 21:30:51 -0700

    Add files via upload

---
+++ Scripts/Behaviors/Face.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Face : Align
+{
+    public Kinematic Target;
+    // TODO: override Align's getTargetAngle to face the target instead of matching it's orientation
+    public override float getTargetAngle()
+    {
+        Vector3 velocity = target.transform.position - character.transform.position; //Compare coordinates to create a direction of motion
+
+        if (velocity.magnitude == 0) //If the character isn't moving they will face in the direction they are currently facing
+        {
+            return character.transform.eulerAngles.y; 
+        }
+        float targetAngle = Mathf.Atan2(velocity.x, velocity.z); //Extrapolate the direction of motion in radians
+        targetAngle *= Mathf.Rad2Deg; //Convert radians to degrees
+
+        return targetAngle; //return the angle to face1
+    }
+}
---
+++ Scripts/Behaviors/Wander.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wander : SteeringBehavior
+{
+    public Kinematic character;
+
+    float maxAcceleration = 1f;
+
+
+    public override SteeringOutput getSteering()
+    {
+        SteeringOutput result = new SteeringOutput();
+        Vector3 targetPosition = getTargetPosition(); //Out
+        if (targetPosition == Vector3.positiveInfinity)//If the vector3 breaks this will keep it from throwing an error
+        {
+            return null;//Pause movement until it is fixed
+        }
+
+        result.linear = targetPosition - character.transform.position;
+
+        // give full acceleration along this direction
+
+        //I know this was obsolete but I was worried about the maxAcceleration
+        result.linear.Normalize();
+        result.linear *= maxAcceleration;
+        
+
+        //result.angular = 0;
+
+
+        return result;
+    }
+
+    protected virtual Vector3 getTargetPosition() //In
+    {
+        //Produces a random coordinate for the wanderer to move to
+        Vector3 WanderSpot = new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10));
+        //Debug.Log(WanderSpot); //I was having trouble with the randoms but work now
+        return WanderSpot;
+    }
+}
